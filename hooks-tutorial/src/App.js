4a244d27f54b3b79bfdd549690f07f2110a65eae
leeyunhome <leeyunhome@daum.net>
2020-04-06 19:59:17 +0900

    upto Chapter8

--- hooks-tutorial/src/App.js
+++ hooks-tutorial/src/App.js
@@ -1,13 +1,14 @@
 import React, { useState } from 'react';
 // import Counter from './Counter';
 import Info from './Info';
+// import Average from './Average';
 
 const App = () => {
   const [visible, setVisible] = useState(false);
   return (
     <div>
       {/* <Counter /> */}
-      <button
+      {/* <button
         onClick={() => {
           setVisible(!visible);
         }}
@@ -15,7 +16,9 @@ const App = () => {
         {visible ? '숨기기' : '보이기'}
       </button>
       <hr />
-      {visible && <Info />}
+      {visible && <Info />} */}
+      <Info />
+      {/* <Average /> */}
     </div>
   );
 };
