bcd4733d8a5ad74a2fece14ea8846cd97a2fbf19
leeyunhome <leeyunhome@daum.net>
2020-04-07 13:42:48 +0900

    upto Chapter10

--- styling-react/src/App.js
+++ styling-react/src/App.js
@@ -1,11 +1,16 @@
 import React, { Component } from 'react';
-import SassComponent from './SassComponent';
+// import SassComponent from './SassComponent';
+// import CSSModule from './CSSModule.jsx'
+import StyledComponent from './StyledComponent';
 
 class App extends Component {
   render() {
     return (
       <div>
-        <SassComponent />
+        {/* <SassComponent /> */}
+        {/* <CSSModule /> */}
+        <StyledComponent />
+        
       </div>
     );
   }
