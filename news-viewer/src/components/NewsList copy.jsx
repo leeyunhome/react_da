5f0f71ec6bccd2c19db40b52bb09d4565dc9e82e
leeyunhome <leeyunhome@daum.net>
2020-04-08 12:31:28 +0900

    upto page405 Chapter15

--- news-viewer/src/components/NewsList copy.jsx
+++ news-viewer/src/components/NewsList copy.jsx
@@ -25,29 +25,37 @@ const NewsListBlock = styled.div`
 // };
 
 const NewsList = ({ category }) => {
-    const [loading, response, error] = usePromise(() => {
-        const query = category === 'all' ? '' : `&category=${category}`;
-        return axios.get(
-            `http://newsapi.org/v2/top-headlines?country=kr${query}&apiKey=b351f7e6d0b940ef9bbb9de4d6e72543`,
-        );
+    const [articles, setArticles] = useState(null);
+    const [loading, setLoading] = useState(false);
+
+    useEffect(() => {
+        // async를 사용하는 함수 따로 선언
+        const fetchData = async () => {
+            setLoading(true);
+            try {
+                const query = category === 'all' ? '' : `&category=${category}`;
+                const response = await axios.get(
+                    `http://newsapi.org/v2/top-headlines?country=kr${query}&apiKey=b351f7e6d0b940ef9bbb9de4d6e72543`,
+                );
+                setArticles(response.data.articles);
+            } catch (e) {
+                console.log(e);
+            }
+            setLoading(false);
+        };
+        fetchData();
     }, [category]);
 
     // 대기 중일 때
     if (loading) {
-        return <NewsListBlock>대기 중...</NewsListBlock>
+        return <NewsListBlock>대기 중...</NewsListBlock>;
     }
-
-    // 아직 response 값이 설정되지 않았을 때
-    if (!response) {
+    // 아직 articles 값이 설정되지 않았을 때
+    if (!articles) {
         return null;
     }
 
-    // 에러가 발생했을 때
-    if (error) {
-        return <NewsListBlock>에러 발생!</NewsListBlock>
-    }
-    // response 값이 유효할 때
-    const { articles } = response.data;
+    // articles 값이 유효할 때
     return (
         <NewsListBlock>
             {articles.map(article => {
