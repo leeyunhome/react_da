832361cd4b9d42b9836b3acbd7ce406e5d0bac76
leeyunhome <leeyunhome@daum.net>
2020-04-08 13:04:47 +0900

    upto Chapter15

--- context-tutorial/src/components/ColorBox.jsx
+++ context-tutorial/src/components/ColorBox.jsx
@@ -1,30 +1,27 @@
-import React from 'react';
-import ColorConsumer from '../contexts/color';
+import React, { useContext } from "react";
+import ColorConsumer from "../contexts/color";
+import ColorContext from "../contexts/color";
 
 const ColorBox = () => {
-    return (
-        <ColorConsumer>
-            {({state}) => (
-                <>
-                    <div
-                        style={{
-                            width: '64px',
-                            height: '64px',
-                            background: state.color
-                        }}
-                    />
-                    <div
-                        style={{
-                            width: '32px',
-                            height: '32px',
-                            background: state.subcolor
-                        }}
-                    />
-                
-                </>
-            )}
-        </ColorConsumer>
-    );
+  const { state } = useContext(ColorContext);
+  return (
+    <>
+      <div
+        style={{
+          width: "64px",
+          height: "64px",
+          background: state.color,
+        }}
+      />
+      <div
+        style={{
+          width: "32px",
+          height: "32px",
+          background: state.subcolor,
+        }}
+      />
+    </>
+  );
 };
 
 export default ColorBox;
