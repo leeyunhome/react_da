51714036334f406d4cb09861fc042b807ed66ec5
leeyunhome <leeyunhome@daum.net>
2020-04-08 17:02:30 +0900

    upto page456 Chapter17

--- context-tutorial/src/components/SelectColors.jsx
+++ context-tutorial/src/components/SelectColors.jsx
@@ -31,7 +31,6 @@ class SelectColors extends Component {
                     onClick={() => this.handleSetColor(color)}
                     onContextMenu={e => {
                         e.preventDefault();
-                        this.handleSetSubcolor(color);
                     }}
                   />
                 ))}
