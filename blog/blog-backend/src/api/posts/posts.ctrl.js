2a12a6b8394e80992612cb3726aaccd10f52bf54
leeyunhome <leeyunhome@daum.net>
2020-04-10 13:05:42 +0900

    upto Chapter22

--- blog/blog-backend/src/api/posts/posts.ctrl.js
+++ blog/blog-backend/src/api/posts/posts.ctrl.js
@@ -1,6 +1,6 @@
 import Post from '../../models/post.jsx';
 import mongoose from 'mongoose';
-import Joi from 'joi';
+import Joi from '@hapi/joi';
 
 const { ObjectId } = mongoose.Types;
 
@@ -24,7 +24,7 @@ export const write = async ctx => {
     });
 
     // 검증하고 나서 검증 실패인 경우 에러 처리
-    const result = Joi.validate(ctx.request.body, schema);
+    const result = schema.validate(ctx.request.body);
     if (result.error) {
         ctx.status = 400;   // Bad Request
         ctx.body = result.error;
@@ -46,9 +46,30 @@ export const write = async ctx => {
 };
 
 export const list = async ctx => {
+    // query는 문자열이기 때문에 숫자로 변환해 주어야 합니다.
+    // 값이 주어지지 않았다면 1을 기본으로 사용합니다.
+    const page = parseInt(ctx.query.page || '1', 10);
+
+    if (page < 1) {
+        ctx.status = 400;
+        return;
+    }
+
     try {
-        const posts = await Post.find().exec();
-        ctx.body = posts;
+        const posts = await Post.find()
+            .sort({_id: -1})
+            .limit(10)
+            .skip((page -1) * 10)
+            .exec();
+        const postCount = await Post.countDocuments().exec();
+        ctx.set('Last-Page', Math.ceil(postCount / 10));
+        ctx.body = posts
+            .map(post => post.toJSON())
+            .map(post => ({
+                ...post,
+                body:
+                    post.body.length < 200 ? post.body : `${post.body.slice(0, 200)}...`,
+            }))
     } catch (e) {
         ctx.throw(500, e);
     }
@@ -88,13 +109,13 @@ export const update = async ctx => {
     });
 
     // 검증하고 나서 검증 실패인 경우 에러 처리
-    const result = Joi.validate(ctx.request.body, schema);
+    const result = schema.validate(ctx.request.body);
     if (result.error) {
         ctx.status = 400;   // Bad Request
         ctx.body = result.error;
         return;
     }
-    
+
     try {
         const post = await Post.findByIdAndUpdate(id, ctx.request.body, {
             new: true,  // 이 값을 설정하면 업데이트된 데이터를 반환합니다.
