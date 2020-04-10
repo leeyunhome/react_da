93155405e6bcf22c725ad1a4c1bb9ff3c674f98f
leeyunhome <leeyunhome@daum.net>
2020-04-10 23:11:21 +0900

    upto page758 Chapter24

---
+++ blog/blog-frontend/src/lib/styles/palette.jsx
@@ -0,0 +1,34 @@
+// source: https://yeun.github.io/open-color/
+const palette = {
+    gray: [
+        '#f8f9fa',
+        '#f1f3f5',
+        '#e9ecef',
+        '#dee2e6',
+        '#ced4da',
+        '#adb5bd',
+        '#868e96',
+        '#495057',
+        '#495057',
+        '#343a40',
+        '#212529',
+    ],
+    cyan: [
+        '#e3fafc',
+        '#c5f6fa',
+        '#99e9f2',
+        '#66d9e8',
+        '#3bc9db',
+    ],
+    yellow: [
+        '#fff9db',
+        '#fff3bf',
+        '#ffec99',
+        '#ffe066',
+        '#ffd43b',
+        '#fcc419',
+        '#fab005',
+    ]
+}
+
+export default palette;
