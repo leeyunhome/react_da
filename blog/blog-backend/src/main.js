ebe2ffd115962c1612396a7ae8e6574caff21af3
leeyunhome <leeyunhome@daum.net>
2020-04-12 18:57:17 +0900

    upto Chapter26

--- blog/blog-backend/src/main.js
+++ blog/blog-backend/src/main.js
@@ -15,7 +15,7 @@ mongoose
 .connect(MONGO_URI, { useNewUrlParser: true, useFindAndModify: false})
   .then(() => {
     console.log('Connected to MongoDB');
-    createFakeData();
+    // createFakeData();
   })
   .catch(e => {
     console.error(e);
--- blog/blog-frontend/src/pages/PostListPage.jsx
+++ blog/blog-frontend/src/pages/PostListPage.jsx
@@ -2,14 +2,18 @@ import React from 'react';
 // import Button from '../components/common/Button';
 // import Header from '../components/common/Header';
 import HeaderContainer from '../containers/common/HeaderContainer';
+import PostListContainer from '../containers/posts/PostListContainer';
+import PaginationContainer from '../containers/posts/PaginationContainer';
+// import PostList from '../components/posts/PostList';
 
 const PostListPage = () => {
-    return (
-        <>
-            <HeaderContainer />
-            <div>안녕하세요.</div>
-        </>
-    );
+  return (
+    <>
+      <HeaderContainer />
+      <PostListContainer />
+      <PaginationContainer />
+    </>
+  );
 };
 
 export default PostListPage;
